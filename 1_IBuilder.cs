553cf436d65339b566b042ec2893853cf87bb824
2020536070 <146832569+2020536070@users.noreply.github.com>
2025-08-28 12:55:08 +0200

    Add files via upload

---
+++ 1_IBuilder.cs
@@ -0,0 +1,15 @@
+﻿/* CSIS2614. 2024
+ * Class test 2
+ * P Blignaut
+ */
+
+using Microsoft.VisualBasic.Devices;
+
+namespace Computers
+{
+    public interface IComputerBuilder
+    {
+        //Property or method to return the object after it has been built
+        Computer GetComputer();
+    } //IComputerBuilder
+}
