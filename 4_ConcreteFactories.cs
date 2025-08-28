31a5f30b1dc60c3d3d883261e6f1e7d0e1107dfc
2020536070 <146832569+2020536070@users.noreply.github.com>
2025-08-28 12:52:49 +0200

    Add files via upload

---
+++ 4_ConcreteFactories.cs
@@ -0,0 +1,21 @@
+﻿/* CSIS2664, 2025
+ * Class test 2, Question 1
+ * Student number:
+ * Surname, Initials:
+ */
+
+class HondaFactory : ACarFactory
+{
+    public override ICar MakeCar(string model, string colour)
+    {
+        return new Honda(model, colour);
+    }
+} //class HondaFactory
+
+class BMWFactory : ACarFactory
+{
+    public override ICar MakeCar(string model, string colour)
+    {
+        return new BMW(model, colour);
+    }
+} //class BMWFactory
